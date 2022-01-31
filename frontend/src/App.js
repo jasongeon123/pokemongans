5ba405f3db5dedf86402aa944e8385bcd56dac8f
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2022-01-30 21:39:56 -0800

    ye

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [getMessage, setGetMessage] = useState({})
+  useEffect(()=>{
+    axios.get('http://localhost:5000/flask/hello').then(response => {
+      setGetMessage(response);
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -48,6 +59,10 @@ function App() {
           <input type="submit" value="Submit"></input>
         </form>
       </header>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
     </div>
   );
 }
